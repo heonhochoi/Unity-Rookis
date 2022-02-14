182b98ff51a704473eb4dfcc3a599d5ad1e9ecda
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-02-14 17:41:23 +0900

    Prefabs
    Nested Prefabs
    Prefab Variant 등 새로 등장한 개념
    Prefab Instance 같이 기본 개념
    잘 숙지하기

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+class Tank
+{
+    public float speed;
+    Player player; // 포함 관계 Nested Prefab 
+}
+
+// Prefab Variant - 상속의 개념
+class FastTank : Tank
+{
+    
+}
+
+
+class Player
+{
+
+}
+
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -22,22 +41,22 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.4f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
             transform.position += (Vector3.forward * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.4f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
             transform.position += (Vector3.back * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.4f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
             transform.position += (Vector3.left * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.4f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             transform.position += (Vector3.right * Time.deltaTime * _speed);
         }
     }

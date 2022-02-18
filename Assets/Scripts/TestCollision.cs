69f23e23711974e433cb5a13659cfcca1eef9d1b
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-02-18 23:03:15 +0900

    RayCasting
    레이저를 쏘는것
    화면의 게임 obj를 클릭하거나
    객체 앞에 뭐가 있는지를 판별할 때 사용됨

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -28,6 +28,15 @@ public class TestCollision: MonoBehaviour
 
     void Update()
     {
-        
+        Vector3 look = transform.TransformDirection(Vector3.forward);   
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+
+        foreach(RaycastHit hit in hits)
+        {
+            Debug.Log($"LayCast {hit.collider.gameObject.name}!");
+        }         
     }
 }

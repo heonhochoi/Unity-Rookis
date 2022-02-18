49edd542285a86a66a197d2b4e5268c5a505282f
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-02-18 21:14:32 +0900

    Trigger
    범위안에 들어왔냐를 판별함
    Collision 표
    왼쪽이 나
    위쪽이 상대

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -9,12 +9,16 @@ public class TestCollision: MonoBehaviour
     // 2. Collider 존재 (나와 상대) - (IsTrigger : OFF) 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision !");
+        Debug.Log($"Collision ! {collision.gameObject.name}");
     }
 
+    /* OnTriggerEnter 작동 조건 */
+    // 1. 둘 다 Collider가 존재해야 한다.
+    // 2. 둘 중 하나는 IsTrigger : On
+    // 3. 둘 중 하나는 RigidBody가 있어야 함.
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger !");
+        Debug.Log($"Trigger ! {other.gameObject.name} ");
     }
 
     void Start()

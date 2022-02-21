7a9fc1c8fd27bb6eb7191bb1ec03afebf0560f70
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-02-21 14:50:55 +0900

    Camera #1
    - Main Camera가 player 따라 다니는 기능
    - Utile폴더 Define에 카메라 모드 정의
    - 플레이어와 카메라 사이의 방향과 거리를 vector를 통해서 정의해둠
    - camera controller 안에 player 객체 넣어둠
    - update문안에 player update와 camera update가 랜덤한 순서로 update되어 버벅거리는 현상을 camera를 LateUpdate함으로써 해결

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -39,12 +39,14 @@ public class TestCollision: MonoBehaviour
 
             Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            //int mask = (1 << 8) | (1 << 9);
+
             RaycastHit hit;
 
-            if(Physics.Raycast(ray,out hit,100.0f))
+            if (Physics.Raycast(ray,out hit,100.0f,mask))
             {
-                Debug.Log($"RayCast Camera @ {hit.collider.gameObject.name}");
-
+                Debug.Log($"RayCast Camera @ {hit.collider.gameObject.tag}");
             }
         }
 

18539bf9b8225e48edde1e8460aba643e731bb18
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-02-26 17:46:24 +0900

    UI 자동화 #1
    UI 자동화 #1
    목표 : GameObject의 이름만 건네주면 알아서 찾아서 UI에 연결
    HOW?
    Bind 함수를 만들어 enum안의
    이름과 겹치는 gameObject를 찾아서 알아서 맵핑 -> Reflection 이용

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,42 @@ using UnityEngine.UI;
 
 public class UI_Button : MonoBehaviour
 {
-    [SerializeField]
-    Text _text;
+    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();  
+
+    enum Buttons
+    {
+        PointButton,
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText,
+
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+    }
+
+    void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] =  Util.FindChild<T>(gameObject, names[i], true);
+        }
+    }
 
     int _score = 0;
 
     public void OnButtonCllicked()
     {
         _score++;
-        _text.text = $"Á¡¼ö : {_score}";
     }
 }

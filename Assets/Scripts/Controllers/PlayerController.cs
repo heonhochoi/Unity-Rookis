ccc20adc48722db00bb16bc2c90f5ead494db574
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-03-06 19:44:31 +0900

    인벤토리 #1
    1. 캔디 에셋 추가
    2. UI Inven ,item 프리팹 생성

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -15,8 +15,6 @@ public class PlayerController : MonoBehaviour
         Managers.Input.mouseAction -= OnMouseClicked; 
         Managers.Input.mouseAction += OnMouseClicked;
 
-        //TEMP
-        Managers.UI.ShowPopupUI<UI_Button>();
     }
 
     public enum Player_state

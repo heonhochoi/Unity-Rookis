21aac8f79a98b55b963809ab006a5d579604195a
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-03-29 16:26:40 +0900

    SoundManager #2
    Sound에 경로주고 Play
    Init 함수를 둬서 Sound라는 빈 obj를 관리하게 둠 이거 DontDestroyOnLoad로 만들어서 계속 관리

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -45,6 +45,7 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(go);
 
             s_instance = go.GetComponent<Managers>();
+            s_instance._sound.Init();
         }
 
     }
--- Assets/Scripts/TestSound.cs
+++ Assets/Scripts/TestSound.cs
@@ -16,14 +16,17 @@ public class TestSound : MonoBehaviour
 
     public AudioClip audioClip;
     public AudioClip audioClip2;
+
     private void OnTriggerEnter(Collider other)
     {
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.PlayOneShot(audioClip);
-        audio.PlayOneShot(audioClip2);
+        //AudioSource audio = GetComponent<AudioSource>();
+        //audio.PlayOneShot(audioClip);
+        //audio.PlayOneShot(audioClip2);
         //float lifeTime = Mathf.Max(audioClip.length, audioClip2.length);
         //GameObject.Destroy(gameObject, lifeTime);
 
-    }
+        Managers.Sound.Play(Define.Sound.Effect, "UnityChan/univ0001");
+        Managers.Sound.Play(Define.Sound.Effect, "UnityChan/univ0002");
 
+    }
 }

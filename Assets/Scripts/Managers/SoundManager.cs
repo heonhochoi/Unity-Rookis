862188efded09ee4efed1eb3282d049a199bf582
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-03-29 14:52:51 +0900

    SoundManager #1
    Audio 재생 시 필요한 요소
    1) MP3 플레이어 	-> Audio Source
    2) MP3 음원 	-> Audio Clip
    3) 귀		-> Audio Listener
    
    Audio Listener는 한 Scene에 하나씩 박혀있는데 보통 카메라에 박혀있다.
    SoundManager script 생성

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager
+{
+    // MP3 Player   -> Audio Source
+    // MP3 music    -> Audio Clip
+    // ±Í           -> Audio Listener
+}

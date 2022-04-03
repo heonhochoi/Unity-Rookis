462a706057327dfd703f679fe61d26017bd657c8
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-04-03 15:31:10 +0900

    Coroutine #1
    Coroutine이란?
    엄청 복잡한 함수를 한 프레임에 해결하려고 하면 문제가 생길 수 있다.
    이러한 함수를 여러 프레임에 걸쳐서 분할해서 해결하고 싶음 -> 매우 어렵다.
    이때 프로그램 상에서 일시 정지후 다음 틱에 그 함수를 이어나가게 해보자
    --> 이게 Coroutine
    정리
    : 함수를 일시 정지 시킨 후 모든 값들을 똑같이 복원시킨 상태로 다시 함수를
    시작하는 기능 (C++에는 없다.)

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameScene : BaseScene
-{       
+{
+    class Test
+    {
+        public int id = 0;
+    }
+
+    class CoroutineTest : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            // for문에서 사용할 때
+            for(int i=0;i<100000;i++)
+            {
+                if (i % 10000 == 0)
+                    yield return null;
+            }
+            // 이렇게 하면 10000번을 기준으로 함수의 제어권을 
+            // 다른 지역함수로 넘길 수 있다. 
+
+            yield return new Test() { id = 1 };
+   
+            // Skip하고 싶을 때
+            yield return null;
+
+            yield return new Test() { id = 2 };
+
+            // 영구 종료 하고 싶을 때 
+            yield break;
+        }
+    }
+
     protected override void Init()
     {
-        base.Init();
+        base.Init(); 
 
         SceneType = Define.Scene.Game;
-
+        
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        for (int i = 0; i < 5; i++)
-            Managers.Resource.Instantiate("UnityChan");
+        CoroutineTest test = new CoroutineTest();
+        foreach(System.Object t in test)
+        {
+            Test _test = (Test)t;
+            int id = _test.id;
+            Debug.Log(id);
+        }
     }
 
     public override void Clear()

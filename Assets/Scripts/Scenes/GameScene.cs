fd31c7a6bfce5966c89d092fd113ccb3e9101ea1
heonhochoi <38308307+heonhochoi@users.noreply.github.com>
2022-04-03 15:45:22 +0900

    Coroutine 메모
    사용법을 익혀보자 #1
             1. 아이템을 드롭하는 경우
            void GenerateItem()
            {
                // 아이템 만들기
                // DB 저장
    
                // 멈춤 -- 이 멈춤을 Coroutine을 사용해 구현하자
                // 그 뒤 로직
            }
    
            사용법을 익혀보자 #2
             2. 시간 관련 기능 ex) 3초후에 터지는 폭탄
             Coroutine을 안 쓸 경우 틱마다 deltatime을 계산 후 펑 -> 너무 부하가 크다...
             Coroutine을 사용해 공동 Time Manager에서 관리해보자!@!

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Coroutine !
+// 1. 함수의 상태를 저장/복원 가능!
+    // -> 엄청 오래걸리는 작업을 잠시 끊거나
+    // -> 원하는 타이밍에 함수를 잠시 멈추고/ 복원 하는 경우
+// 2. return시 -> 우리가 원하는 타입으로 가능 (class도 가능)
 public class GameScene : BaseScene
 {
     class Test
@@ -27,11 +33,11 @@ public class GameScene : BaseScene
             // Skip하고 싶을 때
             yield return null;
 
-            yield return new Test() { id = 2 };
-
             // 영구 종료 하고 싶을 때 
             yield break;
         }
+
+    
     }
 
     protected override void Init()
